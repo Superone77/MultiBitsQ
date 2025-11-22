b1b8b92dd57714c3db18a80c02e9a7e989df88b4
One7 <yangwq177@163.com>
2025-11-22 16:42:28 +0800

    add AsymQuantizer

--- ParetoQ/models/utils_quant.py
+++ ParetoQ/models/utils_quant.py
@@ -245,6 +245,89 @@ class StretchedElasticQuant(torch.autograd.Function):
         grad_input = indicate_middle * grad_output
         return grad_input, grad_alpha, None, None
 
+
+class AsymQuantizer(torch.autograd.Function):
+    """
+    min-max quantization
+    """
+
+    @staticmethod
+    # pyre-fixme[14]: `forward` overrides method defined in `_SingleLevelFunction`
+    #  inconsistently.
+    # pyre-fixme[2]: Parameter must be annotated.
+    def forward(ctx, input, clip_val, num_bits, layerwise) -> Tensor:
+        """
+        :param ctx:
+        :param input: tensor to be quantized
+        :param clip_val: clip the tensor before quantization
+        :param quant_bits: number of bits
+        :return: quantized tensor
+        """
+        ctx.save_for_backward(input, clip_val)
+
+        # input = torch.where(input < clip_val[1], input, clip_val[1])
+        # input = torch.where(input > clip_val[0], input, clip_val[0])
+        # input = torch.clamp(input, clip_val[0], clip_val[1])
+        # NOTE: dynamic scaling gives better performance than static
+        if layerwise:
+            alpha = (input.max() - input.min()).detach()
+            beta = input.min().detach()
+        else:
+            if input.ndimension() <= 3:
+                # weight & hidden layer
+                alpha = (
+                    (
+                        input.max(dim=-1, keepdim=True)[0]
+                        - input.min(dim=-1, keepdim=True)[0]
+                    )
+                    .expand_as(input)
+                    .detach()
+                )
+                beta = input.min(dim=-1, keepdim=True)[0].expand_as(input).detach()
+            elif input.ndimension() == 4:
+                # TODO: attention score matrix, calculate alpha / beta per head
+                tmp = input.view(input.shape[0], input.shape[1], -1)
+                alpha = (
+                    (
+                        tmp.max(dim=-1, keepdim=True)[0].unsqueeze(-1)
+                        - tmp.min(dim=-1, keepdim=True)[0].unsqueeze(-1)
+                    )
+                    .expand_as(input)
+                    .detach()
+                )
+                beta = (
+                    tmp.min(dim=-1, keepdim=True)[0]
+                    .unsqueeze(-1)
+                    .expand_as(input)
+                    .detach()
+                )
+            else:
+                raise ValueError
+        input_normalized = (input - beta) / (alpha + 1e-8)
+        s = 2**num_bits - 1
+        quant_input = torch.round(input_normalized * s).div(s)
+        output = quant_input * (alpha + 1e-8) + beta
+
+        return output
+
+    @staticmethod
+    # pyre-fixme[14]: `backward` overrides method defined in `_SingleLevelFunction`
+    #  inconsistently.
+    # pyre-fixme[3]: Return type must be annotated.
+    # pyre-fixme[2]: Parameter must be annotated.
+    def backward(ctx, grad_output):
+        """
+        :param ctx: saved non-clipped full-precision tensor and clip_val
+        :param grad_output: gradient ert the quantized tensor
+        :return: estimated gradient wrt the full-precision tensor
+        """
+        input, clip_val = ctx.saved_tensors  # unclipped input
+        grad_input = grad_output.clone()
+        grad_input[input.ge(clip_val[1])] = 0
+        grad_input[input.le(clip_val[0])] = 0
+        return grad_input, None, None, None
+
+
 class QuantizeLinear(nn.Linear):
     def __init__(
         self,
@@ -470,11 +553,14 @@ class QuantizeLinear(nn.Linear):
             ):
                 weight_clip_val = weight_clip_val.to(real_weights.device)
 
-            quantizer = (
-                StretchedElasticQuant
-                if w_bits in (0, 2)
-                else LsqBinaryTernaryExtension
-            )
+            if self.multiple_bits_disable_clipvals:
+                quantizer = AsymQuantizer
+            else:
+                quantizer = (
+                    StretchedElasticQuant
+                    if w_bits in (0, 2)
+                    else LsqBinaryTernaryExtension
+                )
             weight = quantizer.apply(
                 real_weights,
                 weight_clip_val,
