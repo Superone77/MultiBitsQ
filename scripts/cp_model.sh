99c68250c81856cb7b47f0816b6f410b77b3894d
One7 <yangwq177@163.com>
2025-12-05 22:11:15 +0800

    add model download and move script

--- scripts/cp_model.sh
+++ scripts/cp_model.sh
@@ -4,7 +4,7 @@ SAVE_DIR="/fast/wangk/"
 
 mkdir -p $WORK_DIR/tmp_model/
 python $WORK_DIR/MultiBitsQ/scripts/download_hfmodel.py --output_dir $WORK_DIR/tmp_model/ --models facebook/MobileLLM-ParetoQ-125M-BF16 --token $HF_TOKEN
-python $WORK_DIR/MultiBitsQ/scripts/download_model.py --output_dir $WORK_DIR/tmp_model/ --models LLM-Research/Llama-3.2-1B-BF16 --token $HF_TOKEN
+python $WORK_DIR/MultiBitsQ/scripts/download_model.py --output_dir $WORK_DIR/tmp_model/ --models LLM-Research/Llama-3.2-1B
 
 
 mv $WORK_DIR/tmp_model/ $SAVE_DIR/MultiBitsQ/model/
