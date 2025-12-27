140c4cbb65a3e0f5b49440b4d3fa61ff9c342d38
One7 <yangwq177@163.com>
2025-12-27 18:59:51 +0800

    add new quantizer

--- ParetoQ/models/utils_quant.py
+++ ParetoQ/models/utils_quant.py
@@ -247,6 +247,584 @@ class StretchedElasticQuant(torch.autograd.Function):
         return grad_input, grad_alpha, None, None
 
 
+class ElasticQuantBinarizerSigned(torch.autograd.Function):
+    """
+    Modified from Learned Step-size Quantization.
+    https://arxiv.org/abs/1902.08153
+    """
+
+    @staticmethod
+    def forward(ctx, input, alpha, num_bits, layerwise):
+        """
+        :param input: input to be quantized
+        :param alpha: the step size
+        :param num_bits: quantization bits
+        :param layerwise: rowwise quant
+        :return: quantized output
+        """
+        ctx.num_bits = num_bits
+        if num_bits >= 16:
+            return input
+        if num_bits == 1 or num_bits == 0:
+            Qn = -1
+            Qp = 1
+        else:
+            Qn = -(2 ** (num_bits - 1))
+            Qp = 2 ** (num_bits - 1) - 1
+
+        eps = torch.tensor(0.00001, device=alpha.device).float()
+        if (alpha - 1).abs().sum() == 0.0:
+            if layerwise:
+                alpha = 2 * input.abs().mean() / math.sqrt(Qp)
+            else:
+                alpha = (
+                    2
+                    * torch.mean(input.abs().mean(0), dim=-1, keepdim=True)
+                    / math.sqrt(Qp)
+                )
+
+        alpha = torch.where(alpha > eps, alpha, eps)
+
+        grad_scale = (
+            1.0 / math.sqrt(input.numel())
+            if not Qp
+            else 1.0 / math.sqrt(input.numel() * Qp)
+        )
+        ctx.save_for_backward(input, alpha)
+        ctx.other = grad_scale, Qn, Qp, layerwise
+        if num_bits == 1:
+            q_w = input.sign()
+        else:
+            q_w = (input / alpha).round().clamp(Qn, Qp)
+        w_q = q_w * alpha
+        return w_q
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if ctx.num_bits >= 16:
+            return grad_output, None, None, None
+
+        input_, alpha = ctx.saved_tensors
+        grad_scale, Qn, Qp, layerwise = ctx.other
+        q_w = input_ / alpha
+        indicate_small = (q_w < Qn).float()
+        indicate_big = (q_w > Qp).float()
+        indicate_middle = (
+            1.0 - indicate_small - indicate_big
+        )  # this is more cpu-friendly than torch.ones(input_.shape)
+        if ctx.num_bits == 1:
+            if layerwise:
+                grad_alpha = (
+                    ((input_.sign()) * grad_output * grad_scale).sum().unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (input_.sign()) * grad_output * grad_scale
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+        else:
+            if layerwise:
+                grad_alpha = (
+                    (
+                        (
+                            indicate_small * Qn
+                            + indicate_big * Qp
+                            + indicate_middle * (-q_w + q_w.round())
+                        )
+                        * grad_output
+                        * grad_scale
+                    )
+                    .sum()
+                    .unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (
+                    (
+                        indicate_small * Qn
+                        + indicate_big * Qp
+                        + indicate_middle * (-q_w + q_w.round())
+                    )
+                    * grad_output
+                    * grad_scale
+                )
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+
+        grad_input = indicate_middle * grad_output
+        return grad_input, grad_alpha, None, None
+
+
+class ElasticQuantBinarizerSignedStretched(torch.autograd.Function):
+    """
+    Modified from Learned Step-size Quantization.
+    https://arxiv.org/abs/1902.08153
+    """
+
+    @staticmethod
+    def forward(ctx, input, alpha, num_bits, layerwise, alpha_stretch):
+        """
+        :param input: input to be quantized
+        :param alpha: the step size
+        :param num_bits: quantization bits
+        :param layerwise: rowwise quant
+        :param alpha_stretch: stretch parameter
+        :return: quantized output
+        """
+        ctx.num_bits = num_bits
+        if num_bits >= 16:
+            return input
+        if num_bits == 1 or num_bits == 0:
+            Qn = -1
+            Qp = 1
+        else:
+            Qn = -(2 ** (num_bits - 1))
+            Qp = 2 ** (num_bits - 1) - 1
+
+        eps = 1e-5
+        if (alpha - 1).abs().sum() == 0.0:
+            if layerwise:
+                alpha = 2 * input.abs().mean() / math.sqrt(Qp)
+            else:
+                alpha = (
+                    2
+                    * torch.mean(input.abs().mean(0), dim=-1, keepdim=True)
+                    / math.sqrt(Qp)
+                )
+
+        alpha = torch.where(alpha > eps, alpha, eps)
+
+        grad_scale = (
+            1.0 / math.sqrt(input.numel())
+            if not Qp
+            else 1.0 / math.sqrt(input.numel() * Qp)
+        )
+        ctx.save_for_backward(input, alpha)
+        ctx.other = grad_scale, Qn, Qp, layerwise
+        if num_bits == 1:
+            q_w = input.sign()
+        else:
+            q_w = (input / alpha).round().clamp(Qn, Qp)
+        w_q = q_w * alpha
+        w_q = input + (w_q - input) * (alpha_stretch)
+        return w_q
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if ctx.num_bits >= 16:
+            return grad_output, None, None, None, None
+
+        input_, alpha = ctx.saved_tensors
+        grad_scale, Qn, Qp, layerwise = ctx.other
+        q_w = input_ / alpha
+        indicate_small = (q_w < Qn).float()
+        indicate_big = (q_w > Qp).float()
+        indicate_middle = (
+            1.0 - indicate_small - indicate_big
+        )  # this is more cpu-friendly than torch.ones(input_.shape)
+        if ctx.num_bits == 1:
+            if layerwise:
+                grad_alpha = (
+                    ((input_.sign()) * grad_output * grad_scale).sum().unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (input_.sign()) * grad_output * grad_scale
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+        else:
+            if layerwise:
+                grad_alpha = (
+                    (
+                        (
+                            indicate_small * Qn
+                            + indicate_big * Qp
+                            + indicate_middle * (-q_w + q_w.round())
+                        )
+                        * grad_output
+                        * grad_scale
+                    )
+                    .sum()
+                    .unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (
+                    (
+                        indicate_small * Qn
+                        + indicate_big * Qp
+                        + indicate_middle * (-q_w + q_w.round())
+                    )
+                    * grad_output
+                    * grad_scale
+                )
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+
+        grad_input = indicate_middle * grad_output
+        return grad_input, grad_alpha, None, None, None
+
+
+class ElasticQuantN2UQ(torch.autograd.Function):
+    """
+    Modified from Learned Step-size Quantization.
+    https://arxiv.org/abs/1902.08153
+    """
+
+    @staticmethod
+    def forward(ctx, input, alpha, num_bits, layerwise):
+        """
+        :param input: input to be quantized
+        :param alpha: the step size
+        :param num_bits: quantization bits
+        :param layerwise: rowwise quant
+        :return: quantized output
+        """
+        ctx.num_bits = num_bits
+        if num_bits >= 16:
+            return input
+        if num_bits == 1 or num_bits == 0:
+            Qn = -1
+            Qp = 1
+        else:
+            Qn = -(2 ** (num_bits - 1))
+            Qp = 2 ** (num_bits - 1) - 1
+
+        eps = torch.tensor(0.00001, device=alpha.device).float()
+        alpha = torch.where(alpha > eps, alpha, eps)
+
+        grad_scale = (
+            1.0 / math.sqrt(input.numel())
+            if not Qp
+            else 1.0 / math.sqrt(input.numel() * Qp)
+        )
+        ctx.save_for_backward(input, alpha)
+        clip_val = 1 - 1e-2
+        if num_bits == 0:
+            n_levels = 1.5
+            shift = 0
+        else:
+            n_levels = 2 ** (num_bits - 1)
+            shift = 0.5
+        Qp = (n_levels - shift) / n_levels
+        Qn = -Qp
+        ctx.other = grad_scale, Qn, Qp, layerwise
+        if num_bits == 1:
+            q_w = input.sign()
+        else:
+            q_w = (
+                torch.round(
+                    torch.clamp(input / alpha, -clip_val, clip_val) * n_levels - shift
+                )
+                + shift
+            ) / n_levels
+        w_q = q_w * alpha
+        return w_q
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if ctx.num_bits >= 16:
+            return grad_output, None, None, None
+
+        input_, alpha = ctx.saved_tensors
+        grad_scale, Qn, Qp, layerwise = ctx.other
+        q_w = input_ / alpha
+        clip_val = 1 - 1e-2
+        if ctx.num_bits == 0:
+            n_levels = 1.5
+            shift = 0
+        else:
+            n_levels = 2 ** (ctx.num_bits - 1)
+            shift = 0.5
+        indicate_small = (q_w < -clip_val).float()
+        indicate_big = (q_w > clip_val).float()
+        indicate_middle = (
+            1.0 - indicate_small - indicate_big
+        )  # this is more cpu-friendly than torch.ones(input_.shape)
+        if ctx.num_bits == 1:
+            if layerwise:
+                grad_alpha = (
+                    ((input_.sign()) * grad_output * grad_scale).sum().unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (input_.sign()) * grad_output * grad_scale
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+        else:
+            if layerwise:
+                grad_alpha = (
+                    (
+                        (
+                            indicate_small * Qn
+                            + indicate_big * Qp
+                            + indicate_middle
+                            * (
+                                -q_w
+                                + (
+                                    torch.round(
+                                        torch.clamp(q_w, -clip_val, clip_val) * n_levels
+                                        - shift
+                                    )
+                                    + shift
+                                )
+                                / n_levels
+                            )
+                        )
+                        * grad_output
+                        * grad_scale
+                    )
+                    .sum()
+                    .unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (
+                    (
+                        indicate_small * Qn
+                        + indicate_big * Qp
+                        + indicate_middle
+                        * (
+                            -q_w
+                            + (
+                                torch.round(
+                                    torch.clamp(q_w, -clip_val, clip_val) * n_levels
+                                    - shift
+                                )
+                                + shift
+                            )
+                            / n_levels
+                        )
+                    )
+                    * grad_output
+                    * grad_scale
+                )
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+
+        grad_input = indicate_middle * grad_output
+        return grad_input, grad_alpha, None, None
+
+
+class ElasticQuantN2UQStreched(torch.autograd.Function):
+    """
+    Modified from Learned Step-size Quantization.
+    https://arxiv.org/abs/1902.08153
+    """
+
+    @staticmethod
+    def forward(ctx, input, alpha, num_bits, layerwise, alpha_stretch):
+        """
+        :param input: input to be quantized
+        :param alpha: the step size
+        :param num_bits: quantization bits
+        :param layerwise: rowwise quant
+        :param alpha_stretch: stretch parameter
+        :return: quantized output
+        """
+        ctx.num_bits = num_bits
+        if num_bits >= 16:
+            return input
+        if num_bits == 1 or num_bits == 0:
+            Qn = -1
+            Qp = 1
+        else:
+            Qn = -(2 ** (num_bits - 1))
+            Qp = 2 ** (num_bits - 1) - 1
+
+        eps = 1e-5
+        alpha = torch.where(alpha > eps, alpha, eps)
+
+        grad_scale = (
+            1.0 / math.sqrt(input.numel())
+            if not Qp
+            else 1.0 / math.sqrt(input.numel() * Qp)
+        )
+        ctx.save_for_backward(input, alpha)
+        clip_val = 1 - 1e-2
+        if num_bits == 0:
+            n_levels = 1.5
+            shift = 0
+        else:
+            n_levels = 2 ** (num_bits - 1)
+            shift = 0.5
+        Qp = (n_levels - shift) / n_levels
+        Qn = -Qp
+        ctx.other = grad_scale, Qn, Qp, layerwise
+        if num_bits == 1:
+            q_w = input.sign()
+        else:
+            q_w = (
+                torch.round(
+                    torch.clamp(input / alpha, -clip_val, clip_val) * n_levels - shift
+                )
+                + shift
+            ) / n_levels
+        w_q = q_w * alpha
+        w_q = input + (w_q - input) * (alpha_stretch)
+        return w_q
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if ctx.num_bits >= 16:
+            return grad_output, None, None, None, None
+
+        input_, alpha = ctx.saved_tensors
+        grad_scale, Qn, Qp, layerwise = ctx.other
+        q_w = input_ / alpha
+        clip_val = 1 - 1e-2
+        if ctx.num_bits == 0:
+            n_levels = 1.5
+            shift = 0
+        else:
+            n_levels = 2 ** (ctx.num_bits - 1)
+            shift = 0.5
+        indicate_small = (q_w < -clip_val).float()
+        indicate_big = (q_w > clip_val).float()
+        indicate_middle = (
+            1.0 - indicate_small - indicate_big
+        )  # this is more cpu-friendly than torch.ones(input_.shape)
+        if ctx.num_bits == 1:
+            if layerwise:
+                grad_alpha = (
+                    ((input_.sign()) * grad_output * grad_scale).sum().unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (input_.sign()) * grad_output * grad_scale
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+        else:
+            if layerwise:
+                grad_alpha = (
+                    (
+                        (
+                            indicate_small * Qn
+                            + indicate_big * Qp
+                            + indicate_middle
+                            * (
+                                -q_w
+                                + (
+                                    torch.round(
+                                        torch.clamp(q_w, -clip_val, clip_val) * n_levels
+                                        - shift
+                                    )
+                                    + shift
+                                )
+                                / n_levels
+                            )
+                        )
+                        * grad_output
+                        * grad_scale
+                    )
+                    .sum()
+                    .unsqueeze(dim=0)
+                )
+            else:
+                grad_alpha = (
+                    (
+                        indicate_small * Qn
+                        + indicate_big * Qp
+                        + indicate_middle
+                        * (
+                            -q_w
+                            + (
+                                torch.round(
+                                    torch.clamp(q_w, -clip_val, clip_val) * n_levels
+                                    - shift
+                                )
+                                + shift
+                            )
+                            / n_levels
+                        )
+                    )
+                    * grad_output
+                    * grad_scale
+                )
+                grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+
+        grad_input = indicate_middle * grad_output
+        return grad_input, grad_alpha, None, None, None
+
+
+class ElasticQuantBinarizerUnsigned(torch.autograd.Function):
+    """
+    Modified from Learned Step-size Quantization.
+    https://arxiv.org/abs/1902.08153
+    """
+
+    @staticmethod
+    def forward(ctx, input, alpha, num_bits, layerwise):
+        """
+        :param input: input to be quantized
+        :param alpha: the step size
+        :param num_bits: quantization bits
+        :param layerwise: rowwise quant
+        :return: quantized output
+        """
+        ctx.num_bits = num_bits
+        Qn = 0
+        Qp = 2 ** (num_bits) - 1
+
+        if num_bits == 1:
+            input_ = input
+            min_val = None
+        else:
+            min_val = input.min().item()
+            input_ = input - min_val
+
+        eps = torch.tensor(0.00001, device=alpha.device).float()
+        if (alpha - 1).abs().sum() == 0.0:
+            if layerwise:
+                alpha = 4 * input.abs().mean() / math.sqrt(Qp)
+            else:
+                alpha = (
+                    4
+                    * torch.mean(input.abs().mean(0), dim=-1, keepdim=True)
+                    / math.sqrt(Qp)
+                )
+
+        alpha = torch.where(alpha > eps, alpha, eps)
+
+        grad_scale = 1.0 / math.sqrt(input.numel() * Qp)
+
+        ctx.save_for_backward(input_, alpha)
+        ctx.other = grad_scale, Qn, Qp, layerwise
+
+        q_w = (input_ / alpha).round().clamp(Qn, Qp)
+        w_q = q_w * alpha
+
+        if num_bits != 1:
+            w_q = w_q + min_val
+
+        return w_q
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input_, alpha = ctx.saved_tensors
+        grad_scale, Qn, Qp, layerwise = ctx.other
+        q_w = input_ / alpha
+        indicate_small = (q_w < Qn).float()
+        indicate_big = (q_w > Qp).float()
+        indicate_middle = (
+            1.0 - indicate_small - indicate_big
+        )  # this is more cpu-friendly than torch.ones(input_.shape)
+
+        if layerwise:
+            grad_alpha = (
+                (
+                    (
+                        indicate_small * Qn
+                        + indicate_big * Qp
+                        + indicate_middle * (-q_w + q_w.round())
+                    )
+                    * grad_output
+                    * grad_scale
+                )
+                .sum()
+                .unsqueeze(dim=0)
+            )
+        else:
+            grad_alpha = (
+                (
+                    indicate_small * Qn
+                    + indicate_big * Qp
+                    + indicate_middle * (-q_w + q_w.round())
+                )
+                * grad_output
+                * grad_scale
+            )
+            grad_alpha = torch.sum(grad_alpha, dim=-1, keepdim=True)
+
+        grad_input = indicate_middle * grad_output
+        return grad_input, grad_alpha, None, None
+
 
 class QuantizeLinear(nn.Linear):
     def __init__(
